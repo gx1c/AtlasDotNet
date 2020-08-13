7d42bac36644123a38313c1410c7cc48a55438ca
gx1c.source@gmail.com <gx1c.source@gmail.com>
2020-08-13 10:37:57 +0200

    first commit of old code

---
+++ AtlasDNC/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace AtlasDNC
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 1;
+            int number2 = 4;
+            Console.WriteLine("Initiating...");
+
+            int smth = addition(number2, number);
+            Console.WriteLine(smth);
+            loopdyloop(number);
+        }
+
+        static void loopdyloop(int num)
+        {
+            Console.WriteLine("Atlas says HI!");
+            return;
+        }
+
+        static int addition(int a, int b)
+        {
+            int result = a + b;
+            return result;
+        }
+
+
+    }
+
+
+}
